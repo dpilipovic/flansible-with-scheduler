496d210b37fc01ae5a950a5a61a7c0a2a7d907fc
dpilipovic <49248453+dpilipovic@users.noreply.github.com>
2019-08-20 14:48:57 -0700

    Update start-flansible-alpine.sh

--- bin/start-flansible-alpine.sh
+++ bin/start-flansible-alpine.sh
@@ -5,5 +5,5 @@ cd /opt/flansible ;
 /opt/flansible/venv/bin/python /opt/flansible/venv/bin/gunicorn -b 0.0.0.0:8002 -k gevent --worker-connections 1000 --timeout 900 run:app -D;
 /opt/flansible/venv/bin/python /opt/flansible/venv/bin/gunicorn -b 0.0.0.0:8003 -k gevent --worker-connections 1000 --timeout 900 run:app -D;
 /usr/bin/redis-server /etc/redis.conf;
-celery beat -A app.celery --loglevel=INFO --logfile=/opt/app/Zanother_celer/flansible/app/logs/server.log --detach;
-celery worker -A app.celery --loglevel=INFO --logfile=/opt/app/Zanother_celer/flansible/app/logs/server.log --detach ;
+/opt/flansible/venv/bin/python /opt/flansible/venv/bin/celery beat -A app.celery --loglevel=INFO --logfile=/opt/flansible/app/logs/server.log --detach;
+/opt/flansible/venv/bin/python /opt/flansible/venv/bin/celery worker -A app.celery --loglevel=INFO --logfile=/opt/flansible/app/logs/server.log --detach ;
