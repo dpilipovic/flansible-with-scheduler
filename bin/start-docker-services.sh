7e2b5aa6605c6e3cf5c4b5422d1d806c350d6da2
dpilipovic <49248453+dpilipovic@users.noreply.github.com>
2019-08-23 11:50:44 -0700

    Update start-docker-services.sh

--- bin/start-docker-services.sh
+++ bin/start-docker-services.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script to start docker services - first gunicorn uwsgi server which starts Flask app
 #/opt/flansible/bin/start-flansible.sh &
-su - flansible -c "/opt/flansible/bin/start-single.sh &"
+#su - flansible -c "/opt/flansible/bin/start-single.sh &"
 
 # and then nginx proxy in front of it
 #nginx -g 'daemon off;' 
